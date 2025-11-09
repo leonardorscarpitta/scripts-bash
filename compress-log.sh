4baaace069089fa610e85cd2c49f033f9c78e943
Leonardo <leonardorscarpitta@gmail.com>
2025-11-09 13:38:47 -0300

    feat: add a compress-logs script

---
+++ compress-log.sh
@@ -0,0 +1,25 @@
+while true; do
+  echo " "
+  echo "-----[ Para arquivos na raiz (/), o script deve ser inicializado com sudo ]-----"
+  echo "-----[ Pressione CTRL + C para encerrar a aplicação ]-----"
+  echo "Digite o nome do diretório do log que deseja comprimir:"
+  read LOG_DIR
+  echo "Onde deseja salvar?"
+  read STORAGE_FILES
+
+  if [ -n LOG_DIR ]; then
+
+    if ! [ -d $STORAGE_FILES ]; then
+      echo "Criação do arquivo ${STORAGE_FILES} para armazenar os logs"
+      mkdir $STORAGE_FILES
+      chown $USER:$USER archived
+    fi
+    COMPRESSED_FOLDER_NAME="${STORAGE_FILES}/${LOG_DIR}_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz"
+    tar -czf $COMPRESSED_FOLDER_NAME $LOG_DIR
+    echo "Arquivo comprimido com sucesso em: ${COMPRESSED_FOLDER_NAME}"
+
+  else
+    clear
+    echo "O diretório não pode estar vazio"
+  fi
+done
